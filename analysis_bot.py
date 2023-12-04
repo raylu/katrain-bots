4b7b1917e7f85844c1f3d9981c1845712ebf8fe2
raylu <90059+raylu@users.noreply.github.com>
2023-12-04 12:38:11 -0800

    double → single quotes

--- analysis_bot.py
+++ analysis_bot.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
 
-"""
-This is a simple python program that demonstrates how to run KataGo's
-analysis engine as a subprocess and send it a query. It queries the
-result of playing the 4-4 point on an empty board and prints out
-the json response.
-"""
-
 import argparse
 import json
 import subprocess
 import time
 from threading import Thread
+from typing import Union, Literal, Any
+
 import sgfmill
 import sgfmill.boards
 import sgfmill.ascii_boards
-from typing import Union, Literal, Any
 
-Color = Union[Literal["b"], Literal["w"]]
-Move = Union[None, Literal["pass"], tuple[int, int]]
+Color = Union[Literal['b'], Literal['w']]
+Move = Union[None, Literal['pass'], tuple[int, int]]
 
 def sgfmill_to_str(move: Move) -> str:
-	if move in (None, "pass"):
-		return "pass"
+	if move in (None, 'pass'):
+		return 'pass'
 	(y, x) = move
-	return "ABCDEFGHJKLMNOPQRSTUVWXYZ"[x] + str(y+1)
+	return 'ABCDEFGHJKLMNOPQRSTUVWXYZ'[x] + str(y+1)
 
 class KataGo:
 	def __init__(self, katago_path: str, config_path: str, model_path: str, additional_args: list[str] = []):
 		self.query_counter = 0
 		katago = subprocess.Popen(
-			[katago_path, "analysis", "-config", config_path, "-model", model_path, *additional_args],
+			[katago_path, 'analysis', '-config', config_path, '-model', model_path, *additional_args],
 			stdin=subprocess.PIPE,
 			stdout=subprocess.PIPE,
 			stderr=subprocess.PIPE,
@@ -41,10 +35,10 @@ class KataGo:
 				data = katago.stderr.readline()
 				time.sleep(0)
 				if data:
-					print("KataGo: ", data.decode(), end="")
+					print('KataGo: ', data.decode(), end='')
 			data = katago.stderr.read()
 			if data:
-				print("KataGo: ", data.decode(), end="")
+				print('KataGo: ', data.decode(), end='')
 		self.stderrthread = Thread(target=printforever)
 		self.stderrthread.start()
 
@@ -55,81 +49,81 @@ class KataGo:
 	def query(self, initial_board: sgfmill.boards.Board, moves: list[tuple[Color, Move]], komi: float, max_visits=None):
 		query = {}
 
-		query["id"] = str(self.query_counter)
+		query['id'] = str(self.query_counter)
 		self.query_counter += 1
 
-		query["moves"] = [(color, sgfmill_to_str(move)) for color, move in moves]
-		query["initialStones"] = []
+		query['moves'] = [(color, sgfmill_to_str(move)) for color, move in moves]
+		query['initialStones'] = []
 		for y in range(initial_board.side):
 			for x in range(initial_board.side):
 				color = initial_board.get(y, x)
 				if color:
-					query["initialStones"].append((color, sgfmill_to_str((y, x))))
-		query["rules"] = "Chinese"
-		query["komi"] = komi
-		query["boardXSize"] = initial_board.side
-		query["boardYSize"] = initial_board.side
-		query["includePolicy"] = True
+					query['initialStones'].append((color, sgfmill_to_str((y, x))))
+		query['rules'] = 'Chinese'
+		query['komi'] = komi
+		query['boardXSize'] = initial_board.side
+		query['boardYSize'] = initial_board.side
+		query['includePolicy'] = True
 		if max_visits is not None:
-			query["maxVisits"] = max_visits
+			query['maxVisits'] = max_visits
 		return self.query_raw(query)
 
 	def query_raw(self, query: dict[str, Any]):
-		self.katago.stdin.write((json.dumps(query) + "\n").encode())
+		self.katago.stdin.write((json.dumps(query) + '\n').encode())
 		self.katago.stdin.flush()
 
 		# print(json.dumps(query))
 
-		line = ""
-		while line == "":
+		line = ''
+		while line == '':
 			if self.katago.poll():
 				time.sleep(1)
-				raise Exception("Unexpected katago exit")
+				raise Exception('Unexpected katago exit')
 			line = self.katago.stdout.readline()
 			line = line.decode().strip()
-			# print("Got: " + line)
+			# print('Got: ' + line)
 		response = json.loads(line)
 
 		# print(response)
 		return response
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	description = """
 	Example script showing how to run KataGo analysis engine and query it from python.
 	"""
 	parser = argparse.ArgumentParser(description=description)
 	parser.add_argument(
-		"--katago-path",
-		help="Path to katago executable",
+		'--katago-path',
+		help='Path to katago executable',
 		required=True,
 	)
 	parser.add_argument(
-		"--config-path",
-		help="Path to KataGo analysis config (e.g. cpp/configs/analysis_example.cfg in KataGo repo)",
+		'--config-path',
+		help='Path to KataGo analysis config (e.g. cpp/configs/analysis_example.cfg in KataGo repo)',
 		required=True,
 	)
 	parser.add_argument(
-		"--model-path",
-		help="Path to neural network .bin.gz file",
+		'--model-path',
+		help='Path to neural network .bin.gz file',
 		required=True,
 	)
 	args = vars(parser.parse_args())
 	print(args)
 
-	katago = KataGo(args["katago_path"], args["config_path"], args["model_path"])
+	katago = KataGo(args['katago_path'], args['config_path'], args['model_path'])
 
 	board = sgfmill.boards.Board(19)
 	komi = 6.5
-	moves = [("b", (3, 3))]
+	moves = [('b', (3, 3))]
 
 	displayboard = board.copy()
 	for color, move in moves:
-		if move != "pass":
+		if move != 'pass':
 			row, col = move
 			displayboard.play(row, col, color)
 	print(sgfmill.ascii_boards.render_board(displayboard))
 
-	print("Query result: ")
+	print('Query result: ')
 	print(katago.query(board, moves, komi))
 
 	katago.close()
